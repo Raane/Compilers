a5a22d03f14a9f9250cfd170697a36e383aa472b
Rune Holmgren <Raane.Holm@gmail.com>
2014-02-16 08:02:37 +0100

    Getting the second tree traverse to pass stage 6

--- PS3/src/bindnames.c
+++ PS3/src/bindnames.c
@@ -4,12 +4,26 @@ char* thisClass;
 
 int bind_default ( node_t *root, int stackOffset)
 {
-	
+    int i;
+    for(i=0;i<root->n_children;i++) {
+        if(root->children[i]!=NULL) {
+            root->children[i]->bind_names(root->children[i], stackOffset+1);
+        }
+    }
+    return 0;
 }
 
 int bind_constant ( node_t *root, int stackOffset)
 {
-	
+    int i;
+    for(i=0;i<root->n_children;i++) {
+        if(root->children[i]!=NULL) {
+            root->children[i]->bind_names(root->children[i], stackOffset+1);
+        }
+    }
+    fprintf ( stderr, "CONSTANT\n" );
+    strings_add(root->label);
+    return 0;
 }
 
 
