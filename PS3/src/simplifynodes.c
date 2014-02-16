7006b395ea64de021ad57cbfd6591ba4c120271e
Rune Holmgren <Raane.Holm@gmail.com>
2014-02-16 05:29:19 +0100

    fixing an error in the function list simplifier

--- PS3/src/simplifynodes.c
+++ PS3/src/simplifynodes.c
@@ -74,12 +74,24 @@ Node_t *simplify_list_with_null ( Node_t *root, int depth )
     if(outputStage == 4)
         fprintf ( stderr, "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
     simplify_children(root, depth);
-
-    Node_t* nonNullChild = root->children[1];
-    free(root->children);
-    root->children = malloc(sizeof(Node_t));
-    root->children[0] = nonNullChild;
-    root->n_children = root->n_children-1;
+    
+    if(root->children[0]==NULL) {
+        Node_t* nonNullChild = root->children[1];
+        free(root->children);
+        root->children = malloc(sizeof(Node_t));
+        root->children[0] = nonNullChild;
+        root->n_children = root->n_children-1;
+    } else if(root->children[0]->nodetype.index==root->nodetype.index) {
+        Node_t** newChildren = malloc((root->children[0]->n_children+1) * sizeof(Node_t));
+        int i=0;
+        for(i=0;i<(root->children[0]->n_children+1);i++) {
+            newChildren[i] = root->children[0]->children[i];
+        }
+        newChildren[root->children[0]->n_children] = root->children[1];
+        root->n_children = root->children[0]->n_children+1;
+        free(root->children);
+        root->children = newChildren;
+    }
 }
 
 
