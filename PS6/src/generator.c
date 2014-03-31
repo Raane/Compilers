277cb9339c1aafc8c00e5760c79d15bb3721ea58
Torbjørn Langland <tl_tl_tl80@hotmail.com>
2014-03-31 14:56:07 +0200

    Fixed 2nd conflict

--- PS6/src/generator.c
+++ PS6/src/generator.c
@@ -389,26 +389,14 @@ void gen_ASSIGNMENT_STATEMENT ( node_t *root, int scopedepth )
 
 	// Left hand side may be a class field, which should be handled in this assignment
 	if(root->children[0]->expression_type.index == CLASS_FIELD_E){
-		<<<<<<< HEAD
-			// Fetching address value from child 1, pushing to top of stack:
-			root->children[0]->children[0]->generate(root->children[0]->children[0], scopedepth);
+		// Fetching address value from child 1, pushing to top of stack:
+		root->children[0]->children[0]->generate(root->children[0]->children[0], scopedepth);
 		// Now popping THIS (a.k.a. the objects address value) from stack into r2:
 		instruction_add(POP, r2, NULL, 0,0);
 		// Now popping result from expression into r1:
 		instruction_add(POP, r1, NULL, 0,0);
 		// Now storing to the address on the heap, based on address from child 1 and offset from child 2:
 		instruction_add(STORE, r1, r2, 0, root->children[0]->children[1]->entry->stack_offset);
-		// Pushing class field access result to stac:
-		=======
-			// Fetching address value from child 1, pushing to top of stack:
-			root->children[0]->children[0]->generate(root->children[0]->children[0], scopedepth);
-		// Now poping from stack into r0:
-		instruction_add(POP, r1, NULL, 0,0);
-		// Now storing to the address on the heap, based on address from child 1 and offset from child 2:
-		instruction_add(STORE, r0, r1, 0, root->children[0]->children[1]->entry->stack_offset);
-		// Pushing class field access result to stac:
-    //instruction_add(PUSH, r0, NULL, 0,0); //TODO: Concider, then keep or delete
-		>>>>>>> e92d5f77425ff2024f94ed98b5ce1867294d4642
 	}
 	// or a variable, handled in previous assignment
 	else{
